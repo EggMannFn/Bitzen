9ce61a11a356d5de8412730003f0896b9abfc6e1
EggMannFn <dauti.desion.05@itisdalmine.edu.it>
2024-05-29 20:01:50 +0200

    wallet funzionante finalmente

--- wallet.php
+++ wallet.php
@@ -9,16 +9,23 @@ if(!isset($_SESSION["login"]) || $_SESSION["login"] != true) {
 
 $id_wallet = $_SESSION["wallet"]["id_wallet"];
 
+$coins = ['BTC', 'BNB', 'ETH', 'SOL', 'XRP'];
+$coinQuantities = ['qBTC', 'qBNB', 'qETH', 'qSOL', 'qXRP'];
+
+$selectedCoin = isset($_POST['coin']) ? $_POST['coin'] : $coins[0];
+$selectedCoinQuantity = $coinQuantities[array_search($selectedCoin, $coins)];
+
 //CHECK DI QUANTO HAI DI BILANCIO
-$sql = "SELECT denaroDemo FROM wallet WHERE id_wallet = $id_wallet";
+$sql = "SELECT denaroDemo, {$selectedCoinQuantity} FROM wallet WHERE id_wallet = $id_wallet";
 $result = $connessione->query($sql);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 if ($row === false) {
     die("Error: Wallet non trovato :(");
 }
 $currentBalance = $row['denaroDemo'];
+$currentCoin = $row[$selectedCoinQuantity];
 
-$apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
+$apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol={$selectedCoin}USDT";
 
 $ch = curl_init();
 
@@ -36,73 +43,82 @@ curl_close($ch);
 $data = json_decode($response, true);
 $price = $data['price'];
 if (isset($price)) {
-    $btcPrice = $data['price'];
-    echo "Prezzo di bitcoin: $btcPrice USD";
+    $coinPrice = $data['price'];
+    echo "Prezzo di {$selectedCoin}: {$coinPrice} USD";
 }
-
-$sql = "SELECT quantita, prezzo FROM transazione WHERE id_wallet = $id_wallet AND moneta = 'BTCUSDT' AND tipologia = 'buy'";
+// Calcola il costo totale di acquisto di Coin
+$sql = "SELECT SUM(quantita * prezzo) AS totalCost FROM transazione WHERE id_wallet = $id_wallet AND tipologia = 'buy' AND moneta = '{$selectedCoin}USDT'";
 $result = $connessione->query($sql);
 $row = $result->fetch(PDO::FETCH_ASSOC);
-if ($row == false) {
-    echo "<br> Transazioni non trovate, compra prima di sapere il guadagno/perdita.";
+if ($row === false) {
+    die("Error: Non riesco a calcolare il costo totale di acquisto di {$selectedCoin}.");
 }
-else {
-    $quantityBought = $row['quantita'];
-    $buyPrice = $row['prezzo'];
-    if ($quantityBought > 0) {
-        $gainLoss = ($btcPrice - $buyPrice) * $quantityBought;
-        echo "Guadagno/perdita di BTC: $gainLoss USD";
-    } else {
-        echo "Non hai Bitcoin nel tuo wallet, quindi non c'è guadagno o perdita.";
-    }
+$totalPurchaseCost = $row['totalCost'];
+
+// Calcola il totale delle vendite di Coin
+$sql = "SELECT SUM(quantita * prezzo) AS totalSales FROM transazione WHERE id_wallet = $id_wallet AND tipologia = 'sell' AND moneta = '{$selectedCoin}USDT'";
+$result = $connessione->query($sql);
+$row = $result->fetch(PDO::FETCH_ASSOC);
+if ($row === false) {
+    die("Error: Non riesco a calcolare il totale delle vendite di {$selectedCoin}.");
 }
+$totalSales = $row['totalSales'];
+
+// Calcola il valore attuale del Coin nel wallet
+$currentValue = $currentCoin * $coinPrice;
+
+// Calcola il guadagno o la perdita
+$gainLoss = $currentValue - ($totalPurchaseCost - $totalSales);
+
+echo "<br>Il tuo storico di guadagno e perdita per {$selectedCoin} è: ".round($gainLoss,2) ."USD";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['buy'])) {
         $quantity = $_POST['quantity'];
 
-        $totalCost = $quantity * $btcPrice;
+        $totalCost = $quantity * $coinPrice;
 
         if ($currentBalance < $totalCost) {
-            die("Non hai abbastanza soldi nel wallet per comprare $quantity BTC.");
+            die("Non hai abbastanza soldi nel wallet per comprare $quantity {$selectedCoin}.");
         }
         $newBalance = $currentBalance - $totalCost;
+        $newCoin = $currentCoin + $quantity;
 
-        // Aggiorna il bilancio
-        $sql = "UPDATE wallet SET denaroDemo = $newBalance WHERE id_wallet = $id_wallet";
+        // Aggiorna il bilancio e la quantità di Coin
+        $sql = "UPDATE wallet SET denaroDemo = $newBalance, {$selectedCoinQuantity} = $newCoin WHERE id_wallet = $id_wallet";
         if ($connessione->query($sql) != TRUE) {
             die("Error: " . $sql . "<br>" . $connessione->error);
         }
 
         // Inserisci la transazione
-        $sql = "INSERT INTO transazione (id_transazione, id_wallet, moneta, quantita, tipologia, prezzo, timestamp) VALUES (null, $id_wallet, 'BTCUSDT', $quantity, 'buy', $btcPrice, CURRENT_TIMESTAMP)";
+        $sql = "INSERT INTO transazione (id_transazione, id_wallet, moneta, quantita, tipologia, prezzo, timestamp) VALUES (null, $id_wallet, '{$selectedCoin}USDT', $quantity, 'buy', $coinPrice, CURRENT_TIMESTAMP)";
 
         if ($connessione->query($sql) == TRUE) {
             header("Location: wallet.php");
             exit;
-        }
+        } 
         else{
             echo "Error: " . $sql . "<br>" . $connessione->error;
         }
-    } elseif (isset($_POST['sell'])) {
+    } else if (isset($_POST['sell'])) {
         $quantity = $_POST['quantity'];
 
-        // Verifica se l'utente ha abbastanza Bitcoin
-        if ($quantityBought < $quantity) {
-            die("Non hai abbastanza Bitcoin nel wallet per vendere $quantity BTC.");
+        if ($currentCoin < $quantity) {
+            die("Non hai abbastanza {$selectedCoin} nel wallet per vendere $quantity {$selectedCoin}.");
         }
 
-        $totalReturn = $quantity * $btcPrice;
-        $newBalance = $currentBalance + $totalReturn;
+        $totalRevenue = $quantity * $coinPrice;
+        $newBalance = $currentBalance + $totalRevenue;
+        $newCoin = $currentCoin - $quantity;
 
-        // Aggiorna il bilancio
-        $sql = "UPDATE wallet SET denaroDemo = $newBalance WHERE id_wallet = $id_wallet";
+        // Aggiorna il bilancio e la quantità di Coin
+        $sql = "UPDATE wallet SET denaroDemo = $newBalance, {$selectedCoinQuantity} = $newCoin WHERE id_wallet = $id_wallet";
         if ($connessione->query($sql) != TRUE) {
             die("Error: " . $sql . "<br>" . $connessione->error);
         }
 
         // Inserisci la transazione
-        $sql = "INSERT INTO transazione (id_transazione, id_wallet, moneta, quantita, tipologia, prezzo, timestamp) VALUES (null, $id_wallet, 'BTCUSDT', $quantity, 'sell', $btcPrice, CURRENT_TIMESTAMP)";
+        $sql = "INSERT INTO transazione (id_transazione, id_wallet, moneta, quantita, tipologia, prezzo, timestamp) VALUES (null, $id_wallet, '{$selectedCoin}USDT', $quantity, 'sell', $coinPrice, CURRENT_TIMESTAMP)";
 
         if ($connessione->query($sql) == TRUE) {
             header("Location: wallet.php");
@@ -114,14 +130,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-echo "<h2>Acquista o Vendi Bitcoin</h2>";
-echo "<p>nel wallet hai: </p>";
-echo "<p>$currentBalance USD</p>";
+echo "";
+echo "<p>nel wallet hai: $currentBalance$ </p>";
+echo "<p>Hai $currentCoin {$selectedCoin}</p>";
 ?>
-
+<h2>Acquista</h2>
 <form method="post" action="">
-    <label for="quantity">Quantità da comprare/vendere:</label><br>
+    <label for="coin">Seleziona la moneta:</label><br>
+    <select name="coin" id="coin" onchange="this.form.submit()">
+        <?php foreach ($coins as $coin) : ?>
+            <option value="<?php echo $coin; ?>" <?php echo $coin == $selectedCoin ? 'selected' : ''; ?>><?php echo $coin; ?></option>
+        <?php endforeach; ?>
+    </select><br>
+    <label for="quantity">Quantità da comprare:</label><br>
     <input type="number" id="quantity" name="quantity" min="0.0001" step="0.0001" required><br>
     <input type="submit" name="buy" value="Compra">
+</form>
+
+<h2>Vendi</h2>
+
+<form method="post" action="">
+    <label for="coin">Seleziona la moneta:</label><br>
+    <select name="coin" id="coin" onchange="this.form.submit()">
+        <?php foreach ($coins as $coin) : ?>
+            <option value="<?php echo $coin; ?>" <?php echo $coin == $selectedCoin ? 'selected' : ''; ?>><?php echo $coin; ?></option>
+        <?php endforeach; ?>
+    </select><br>
+    <label for="quantity">Quantità da vendere:</label><br>
+    <input type="number" id="quantity" name="quantity" min="0.0001" step="0.0001" required><br>
     <input type="submit" name="sell" value="Vendi">
 </form>
