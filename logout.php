640037992f07a133ef0d764cd7fc1f89936fb08e
Moncayo5Bi <moncayosaltos.alessandromatteo.05@itisdalmine.edu.it>
2024-05-30 10:51:06 +0200

    fix

--- logout.php
+++ logout.php
@@ -1,8 +1,25 @@
 <?php
 
+// session_start();
+// if
+// session_destroy();
+
+// header("Location: login.php");
+
+?>
+<?php
 session_start();
-session_destroy();
 
-header("Location: login.php");
+//Distruggo la sessione
+if(isset($_POST["logout"])) {
+    session_destroy();
 
+    $cookie_expiry = time()-1;
+
+    setcookie("email", $email, $cookie_expiry);
+    setcookie("password", $password, $cookie_expiry);
+
+    header("Refresh:0;url=./login/login.php");
+    exit;
+}
 ?>
